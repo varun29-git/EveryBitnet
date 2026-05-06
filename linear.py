f7b61585a6dc1ae3d36bcaa8d5c5b660eca2c7c9
varun29-git <vjdaiya29@gmail.com>
2026-05-06 17:18:20 +0530

    Update linear.py

--- linear.py
+++ linear.py
@@ -1,8 +1,10 @@
 from .first_principles import Module, ClassicNeuron as Neuron
+from .first_principles import BitnetNeuron as BN
 
 class Linear(Module):
-    def __init__(self, nin, nout, **kwargs):
-        self.neurons = [Neuron(nin, **kwargs) for _ in range(nout)]
+    def __init__(self, nin, nout, quant_type, **kwargs):
+        neuron_cls = Neuron if (quant_type == "linear") else BN
+        self.neurons = [neuron_cls(nin, **kwargs) for _ in range(nout)] 
         
     def __call__(self, x):
         out = [n(x) for n in self.neurons]
